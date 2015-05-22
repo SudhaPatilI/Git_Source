6fc20b57f9462754370b5b12a1f000d6cb842d56
SudhaPatilI <spiosdeveloper@gmail.com>
2015-05-22 12:52:05 +0530

    New Commit
    Committing

--- Git_Source/ViewController.m
+++ Git_Source/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     NSLog(@"hello");
     NSLog(@"New Push");
+    NSLog(@"New Data");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
